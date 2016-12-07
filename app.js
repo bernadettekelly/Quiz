6bdc35be60c166f91807c1ee0a8a564b355e62fb
bernadettekelly <bernadettemkelly54@gmail.com>
2016-12-06 20:56:35 -0500

    commit

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-$('.page-1').hide(function('.page-2', '.page-3', '.page-4', '.page-5', '.page-6', '.page-7', .page-8') {
-});
 
 $('.start').click(function() {
 	$('.page').hide();
 	$('.page-1').show();
-});
-
-$('.page-2').hide(function('.page-1', '.page-3', '.page-4', '.page-5', '.page-6', '.page-7', .page-8') {
 });
 
 $('.next1').submit(function() {
@@ -14,58 +9,48 @@ $('.next1').submit(function() {
 	$('.page-2').show();
 });
 
-$('.page-3').hide(function('.page-1', '.page-2', '.page-4', '.page-5', '.page-6', '.page-7', .page-8') {
-});
-
 $('.next2').submit(function() {
-	$('page-3').hide();
-});
-
-$('.page-4').hide(function('.page-1', '.page-2', '.page-3', '.page-5', '.page-6', '.page-7', .page-8') {
+	$('.page').hide();
+	$('.page-3').show();
 });
 
 $('.next3').submit(function() {
-	$('page-4').hide();
-});
-
-$('.page-5').hide(function('.page-1', '.page-2', '.page-3', '.page-'4, '.page-6', '.page-7', .page-8') {
+	$('page').hide();
+	$('page-4').show();
 });
 
 $('.next4').submit(function() {
-	$('page-5').hide();
-});
-
-$('.page-6').hide(function('.page-1', '.page-2', '.page-3', '.page-4', '.page-5', '.page-7', .page-8') {
+	$('page').hide();
+	$('page-5').show();
 });
 
 $('.next5').submit(function() {
-	$('page-6').hide();
-});
-
-$('.page-7').hide(function('.page-1', '.page-2', '.page-3', '.page-4', '.page-5', '.page-6', '.page-8') {
-});
-
-$('.next6').submit(function() {
-	$('page-7').hide();
-});
-
-$('.page-8').hide(function('.page-1', '.page-2', '.page-3', '.page-4', '.page-5', '.page-6', '.page-7') {
+	$('page').hide();
+	$('page-6').show();
 });
 
 $('.next6').submit(function() {
-	$('page-7').hide();
+	$('page').hide();
+	$('page-7').show();
+});
+
+$('.repeat').submit(function() {
+	$('page').hide();
+	$('page-8').show();
 });
 
 var totalCorrect = 0;
 var totalIncorrect = 0;
 
 $('.total-correct').text(totalCorrect);
+$('.total-incorrect').text(totalIncorrect);
 
-//$('#quiz-form-2 button[type="button"]').click(function(event){
+$('#quiz-form-2 button[type="button"]').click(function(event){
+
 $('#quiz-form-2').submit(function(event) {
 	event.preventDefault();
-	var choosenOne = $(event.currentTarget).attr('class');
-	if(choosenOne == 'button-A'){
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-A'){
 		totalCorrect++;
 		$('.total-correct').text(totalCorrect);
 	}
@@ -73,144 +58,92 @@ $('#quiz-form-2').submit(function(event) {
 		totalIncorrect++;
 		$('.total-incorrect').text(totalIncorrect);
 	}
-	$('#quiz-form-2 button[type="button"]').addClass('red-on');
-	$('#quiz-form-2 .button-A').addClass('green-on');
+	$('#quiz-form-2 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-2 .button-A').addClass('.green-on');
 });
-
-
-
-$('#quiz-form-2').submit(function(event) {
-	event.preventDefault();
-});
+}
+}
 
 $('#quiz-form-3').submit(function(event) {
 	event.preventDefault();
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-C'){
+		totalCorrect++;
+		$('.total-correct').text(totalCorrect);
+	}
+	else{
+		totalIncorrect++;
+		$('.total-incorrect').text(totalIncorrect);
+	}
+	$('#quiz-form-3 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-3 .button-C').addClass('.green-on');
 });
 
 $('#quiz-form-4').submit(function(event) {
 	event.preventDefault();
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-B'){
+		totalCorrect++;
+		$('.total-correct').text(totalCorrect);
+	}
+	else{
+		totalIncorrect++;
+		$('.total-incorrect').text(totalIncorrect);
+	}
+	$('#quiz-form-4 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-4 .button-B').addClass('.green-on');
 });
 
 $('#quiz-form-5').submit(function(event) {
 	event.preventDefault();
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-D'){
+		totalCorrect++;
+		$('.total-correct').text(totalCorrect);
+	}
+	else{
+		totalIncorrect++;
+		$('.total-incorrect').text(totalIncorrect);
+	}
+	$('#quiz-form-5 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-5 .button-D').addClass('.green-on');
 });
 
 $('#quiz-form-6').submit(function(event) {
 	event.preventDefault();
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-A'){
+		totalCorrect++;
+		$('.total-correct').text(totalCorrect);
+	}
+	else{
+		totalIncorrect++;
+		$('.total-incorrect').text(totalIncorrect);
+	}
+	$('#quiz-form-6 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-6 .button-A').addClass('.green-on');
 });
 
 $('#quiz-form-7').submit(function(event) {
 	event.preventDefault();
+	var chooseOne = $(event.currentTarget).attr('class');
+	if(chooseOne == '.button-C'){
+		totalCorrect++;
+		$('.total-correct').text(totalCorrect);
+	}
+	else{
+		totalIncorrect++;
+		$('.total-incorrect').text(totalIncorrect);
+	}
+	$('#quiz-form-7 button[type="button"]').addClass('.red-on');
+	$('#quiz-form-7 .button-C').addClass('.green-on');
 });
 
-function colors() {
-	return (['green', 'red']);
-}
+$('#quiz-form-8').submit(function(event) {
+	event.preventDefault();
+});
 
-function colorAnswer1() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$('#quiz-form-2 .button-A').addClass('.green-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-2.'.button-B').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-2.'.button-C').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-2.'.button-D').addClass('.red-on');
-	}
-	// If wrong answer is chosen, can I put selected button as red, as well as correct button to turn green so they know what the right answer is?
-
-	function colorAnswer2() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$(#quiz-form-3.'.button-A').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-3.'.button-B').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-3.'.button-C').addClass('.green-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-3.'.button-D').addClass('.red-on');
-	}
-
-	function colorAnswer3() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$(#quiz-form-4.'.button-A').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-4.'.button-B').addClass('.green-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-4.'.button-C').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-4.'.button-D').addClass('.red-on');
-	}
-
-	function colorAnswer4() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$(#quiz-form-5.'.button-A').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-5.'.button-B').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-5.'.button-C').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-5.'.button-D').addClass('.green-on');
-	}
-
-	function colorAnswer5() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$(#quiz-form-6.'.button-A').addClass('.green-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-6.'.button-B').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-6.'.button-C').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-6.'.button-D').addClass('.red-on');
-	}
-
-	function colorAnswer6() {
-	var selectedAnswer = colors();
-	if (selectedAnswer === '.button-A') {
-		$(#quiz-form-7.'.button-A').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-B') {
-		$(#quiz-form-7.'.button-B').addClass('.red-on');
-	}
-	if (selectedAnswer === '.button-C') {
-		$(#quiz-form-7.'.button-C').addClass('.green-on');
-	}
-	if (selectedAnswer === '.button-D') {
-		$(#quiz-form-7.'.button-D').addClass('.red-on');
-	}
-
-	function sortAnswers(answers) {
-		var given = [];
-		for (var i=0; i<answers.length; i++) {
-			if (answers[i]) === '.green-on')
-             given.push(answers[i]);
-            if (answers[i]) === '.red-on')
-             given.push(answers[i]); 
-             // Will it know where to push the answer? How do I set green as correct and red as incorrect?
-
-		}
-
-
-	}
+ }
 	
 }
 
